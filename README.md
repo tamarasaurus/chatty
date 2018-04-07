9c0465d76760d9aa9bb1751def69794cbe37377b
tamarasaurus <tamara@robichet.com>
2018-04-08 01:08:41 +0200

    Add reconnecting websockets

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # chatty
 realtime chat inspired by 2000s ICQ
+
+### tasks
+
+- [ ] Persist connections
+- [ ] Add different colors for uncommitted lines
+- [ ] Usernames
+- [ ] Page style
+- [ ] Add simple color formatting 
--- client/index.js
+++ client/index.js
@@ -1,30 +1,32 @@
-$(function() {
-    const socket = new WebSocket('ws://192.168.1.99:8080');
+/* global ReconnectingWebSocket, _, $ */
 
-    socket.addEventListener('open', function (event) {
-        socket.send('new user joined');
-    });
+$(function () {
+  const socket = new ReconnectingWebSocket('ws://192.168.1.99:8080')
 
-    socket.addEventListener('message', function (event) {
-        const messageGroups = JSON.parse(event.data);
-        const sortedMessages = _.sortBy(messageGroups, 'date');
-        const messageContents = _.map(sortedMessages, 'message');
-        const lines = _.without(messageContents, '\n');
-        
-        const list = $('ul');
-        list.empty()
-        
-        _.each(lines, line => list.append(`<li>${line}</li>`))
-    });
+  socket.onopen = function (e) {
+    console.log('WebSocketClient connected:', e)
+    socket.send('New user joined')
+  }
 
-    $('input').on('keyup', (event) => {
-        const el = $(event.currentTarget);
+  socket.onmessage = function (event, flags, number) {
+    const messageGroups = JSON.parse(event.data)
+    const sortedMessages = _.sortBy(messageGroups, 'date')
+    const messageContents = _.map(sortedMessages, 'message')
+    const lines = _.without(messageContents, '\n')
+    const list = $('ul')
+    list.empty()
 
-        if (event.keyCode === 13) {
-            socket.send('\n');
-            return el.val('');
-        }
+    _.each(lines, line => list.append(`<li>${line}</li>`))
+  }
 
-        socket.send(el.val());
-    })
-});
+  $('input').on('keyup', (event) => {
+    const el = $(event.currentTarget)
+
+    if (event.keyCode === 13) {
+      socket.send('\n')
+      return el.val('')
+    }
+
+    socket.send(el.val())
+  })
+})
