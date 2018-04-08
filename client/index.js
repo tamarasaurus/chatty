36ca2193fc745ab4c0df80e8ce9a01408d54a066
tamarasaurus <tamara@robichet.com>
2018-04-08 12:01:26 +0200

    Fix reconnect and add usernames

--- client/index.js
+++ client/index.js
@@ -1,11 +1,12 @@
 /* global ReconnectingWebSocket, _, $ */
 
 $(function () {
+  let username = null
+
   const socket = new ReconnectingWebSocket('ws://192.168.1.99:8080')
 
   socket.onopen = function (e) {
     console.log('WebSocketClient connected:', e)
-    socket.send('New user joined')
   }
 
   socket.onmessage = function (event, flags, number) {
@@ -19,7 +20,7 @@ $(function () {
     _.each(lines, line => list.append(`<li>${line}</li>`))
   }
 
-  $('input').on('keyup', (event) => {
+  $('.chat input').on('keyup', event => {
     const el = $(event.currentTarget)
 
     if (event.keyCode === 13) {
@@ -27,6 +28,15 @@ $(function () {
       return el.val('')
     }
 
-    socket.send(el.val())
+    socket.send(`${username}: ${el.val()}`)
+  })
+
+  $('.intro button').on('click', event => {
+    const value = $('.intro input').val()
+    $('.intro').hide()
+    $('.chat').show()
+
+    username = value
+    socket.send(`${username} joined`)
   })
 })
--- server/start.js
+++ server/start.js
@@ -1,29 +1,36 @@
-const express = require('express');
-const uuid = require('uuid');
-const http = require('http');
-const url = require('url');
-const WebSocket = require('ws');
-const app = express();
+const express = require('express')
+const uuid = require('uuid')
+const http = require('http')
+const WebSocket = require('ws')
+const app = express()
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const server = http.createServer(app)
+const wss = new WebSocket.Server({ server })
 app.use(express.static('client'))
 
-const clients = [];
-const events = [];
-const messages = {};
+let clients = []
+const messages = {}
 
 wss.on('connection', ws => {
-    ws.id = uuid.v4();
-    clients.push(ws);
+  ws.id = uuid.v4()
+  clients.push(ws)
 
-    ws.on('message', (message) => {
-        if (message === '\n') ws.id = uuid.v4();
-        messages[ws.id] = { id: ws.id, message, date: new Date() }
-        clients.forEach(client => client.send(JSON.stringify(messages)));
-    });
-});
+  ws.on('message', message => {
+    if (message === '\n') ws.id = uuid.v4()
+    messages[ws.id] = {
+      id: ws.id,
+      message,
+      date: new Date()
+    }
 
-server.listen(8080, function listening() {
-  console.log('Listening on %d', server.address().port);
-});
+    clients = clients.filter(client => client.readyState === 1)
+
+    clients.forEach(client => {
+      client.send(JSON.stringify(messages))
+    })
+  })
+})
+
+server.listen(8080, function listening () {
+  console.log('Listening on %d', server.address().port)
+})
